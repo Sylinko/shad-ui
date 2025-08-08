2f218519c7eb33eac6aa49eb3e48fe3d92b905e7
Dear.Va <dear.va.love@gmail.com>
2025-08-09 01:01:16 +0800

    docs: add XML comment

--- src/ShadUI/Controls/ContentExpandControl.axaml.cs
+++ src/ShadUI/Controls/ContentExpandControl.axaml.cs
@@ -4,20 +4,35 @@ using Avalonia.Layout;
 
 namespace ShadUI.Controls;
 
+/// <summary>
+/// A ContentControl that expands its content by a multiplier in a given orientation.
+/// </summary>
 public class ContentExpandControl : ContentControl
 {
+    /// <summary>
+    /// The multiplier to apply to the measured size of the content.
+    /// </summary>
     public static readonly StyledProperty<double> MultiplierProperty =
         AvaloniaProperty.Register<ContentExpandControl, double>(nameof(Multiplier));
 
+    /// <summary>
+    /// The multiplier to apply to the measured size of the content.
+    /// </summary>
     public double Multiplier
     {
         get => GetValue(MultiplierProperty);
         set => SetValue(MultiplierProperty, value);
     }
-        
+
+    /// <summary>
+    /// The orientation to apply the multiplier to.
+    /// </summary>
     public static readonly StyledProperty<Orientation> OrientationProperty =
         AvaloniaProperty.Register<ContentExpandControl, Orientation>(nameof(Orientation));
-        
+
+    /// <summary>
+    /// The orientation to apply the multiplier to.
+    /// </summary>
     public Orientation Orientation
     {
         get => GetValue(OrientationProperty);
@@ -31,12 +46,14 @@ public class ContentExpandControl : ContentControl
         AffectsMeasure<ContentExpandControl>(MultiplierProperty, OrientationProperty);
     }
 
+    /// <inheritdoc />
     protected override Size MeasureCore(Size availableSize)
     {
         var result = base.MeasureCore(availableSize);
         return result;
     }
 
+    /// <inheritdoc />
     protected override Size ArrangeOverride(Size finalSize)
     {
         var result = base.ArrangeOverride(finalSize);
@@ -45,6 +62,7 @@ public class ContentExpandControl : ContentControl
         return result;
     }
 
+    /// <inheritdoc />
     protected override Size MeasureOverride(Size availableSize)
     {
         var result = base.MeasureOverride(availableSize);
