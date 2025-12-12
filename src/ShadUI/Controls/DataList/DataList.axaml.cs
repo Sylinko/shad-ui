d380737b377f3066c9bccce809150a4c4218ab9c
Dear.Va <dear.va.love@gmail.com>
2025-12-12 12:54:09 +0800

    feat: DataList

---
+++ src/ShadUI/Controls/DataList/DataList.axaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Interactivity;
+
+namespace ShadUI.Controls;
+
+[TemplatePart("PART_AddButton", typeof(Button))]
+public class DataList : ListBox
+{
+    /// <summary>
+    /// Defines the <see cref="AddCommand"/> property.
+    /// </summary>
+    public static readonly StyledProperty<ICommand?> AddCommandProperty =
+        AvaloniaProperty.Register<DataList, ICommand?>(nameof(AddCommand));
+
+    /// <summary>
+    /// Gets or sets the command to execute when the add button is clicked.
+    /// </summary>
+    public ICommand? AddCommand
+    {
+        get => GetValue(AddCommandProperty);
+        set => SetValue(AddCommandProperty, value);
+    }
+
+    /// <summary>
+    /// Defines the <see cref="RemoveCommand"/> property.
+    /// </summary>
+    public static readonly StyledProperty<ICommand?> RemoveCommandProperty =
+        AvaloniaProperty.Register<DataList, ICommand?>(nameof(RemoveCommand));
+
+    /// <summary>
+    /// Gets or sets the command to execute when the remove button is clicked.
+    /// Provides the item index to remove as the command parameter.
+    /// </summary>
+    public ICommand? RemoveCommand
+    {
+        get => GetValue(RemoveCommandProperty);
+        set => SetValue(RemoveCommandProperty, value);
+    }
+
+    private IDisposable? _addButtonClickSubscription;
+
+    static DataList()
+    {
+        ItemsSourceProperty.OverrideMetadata<DataList>(new StyledPropertyMetadata<IEnumerable?>(enableDataValidation: true));
+    }
+
+    protected override void UpdateDataValidation(
+        AvaloniaProperty property,
+        BindingValueType state,
+        Exception? error)
+    {
+        if (property == ItemsSourceProperty)
+        {
+            DataValidationErrors.SetError(this, error);
+        }
+    }
+
+    protected override Control CreateContainerForItemOverride(object? item, int index, object? recycleKey)
+    {
+        return new DataListItem();
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        _addButtonClickSubscription?.Dispose();
+
+        if (e.NameScope.Find<Button>("PART_AddButton") is { } addButton)
+        {
+            _addButtonClickSubscription = addButton.AddDisposableHandler(Button.ClickEvent, HandleAddButtonClick);
+        }
+    }
+
+    private void HandleAddButtonClick(object? sender, RoutedEventArgs e)
+    {
+        if (AddCommand is not { } addCommand) return;
+        if (!addCommand.CanExecute(null)) return;
+        addCommand.Execute(null);
+    }
+}
---
+++ src/ShadUI/Controls/DataList/DataListItem.axaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+
+namespace ShadUI.Controls;
+
+[TemplatePart("PART_RemoveButton", typeof(Button))]
+public class DataListItem : ListBoxItem
+{
+    private IDisposable? _removeButtonClickSubscription;
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        _removeButtonClickSubscription?.Dispose();
+
+        if (e.NameScope.Find<Button>("PART_RemoveButton") is { } removeButton)
+        {
+            _removeButtonClickSubscription = removeButton.AddDisposableHandler(Button.ClickEvent, HandleAddButtonClick);
+        }
+    }
+
+    private void HandleAddButtonClick(object? sender, RoutedEventArgs e)
+    {
+        if (ItemsControl.ItemsControlFromItemContainer(this) is not DataList { RemoveCommand: { } removeCommand } dataList) return;
+        var index = dataList.IndexFromContainer(this);
+        if (removeCommand.CanExecute(index)) removeCommand.Execute(index);
+    }
+}
