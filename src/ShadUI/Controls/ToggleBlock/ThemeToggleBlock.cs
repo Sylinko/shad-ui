d2b15989901e8d0ffe64fde234039315e5e1d834
Dear.Va <dear.va.love@gmail.com>
2025-12-19 21:02:36 +0800

    feat: Theme ToggleBlock

---
+++ src/ShadUI/Controls/ToggleBlock/ThemeToggleBlock.cs
@@ -0,0 +1,77 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
+
+namespace ShadUI;
+
+/// <summary>
+/// A toggle block control that switches content based on the theme variant.
+/// </summary>
+public class ThemeToggleBlock : ContentControl
+{
+    public static readonly StyledProperty<IControlTemplate?> DarkContentProperty =
+        AvaloniaProperty.Register<ThemeToggleBlock, IControlTemplate?>(nameof(DarkContent));
+
+    public IControlTemplate? DarkContent
+    {
+        get => GetValue(DarkContentProperty);
+        set => SetValue(DarkContentProperty, value);
+    }
+
+    public static readonly StyledProperty<IControlTemplate?> LightContentProperty =
+        AvaloniaProperty.Register<ThemeToggleBlock, IControlTemplate?>(nameof(LightContent));
+
+    public IControlTemplate? LightContent
+    {
+        get => GetValue(LightContentProperty);
+        set => SetValue(LightContentProperty, value);
+    }
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+
+        ApplyContent(ActualThemeVariant);
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property.Name == "ActualThemeVariant" && change.NewValue is ThemeVariant themeVariant)
+        {
+            ApplyContent(themeVariant);
+        }
+    }
+
+    private void ApplyContent(ThemeVariant? themeVariant)
+    {
+        switch (themeVariant?.Key)
+        {
+            case "Light":
+            {
+                Content = BuildContent(LightContent);
+                break;
+            }
+            case "Dark":
+            {
+                Content = BuildContent(DarkContent);
+                break;
+            }
+            default:
+            {
+                Content = null;
+                break;
+            }
+        }
+
+        Control? BuildContent(IControlTemplate? template)
+        {
+            var result = template?.Build(this)?.Result;
+            result?.DataContext = DataContext;
+            return result;
+        }
+    }
+}
