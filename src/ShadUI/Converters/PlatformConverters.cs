6f5c4cb8cf52020c7de33f6045f7446a43b9487f
Dear.Va <dear.va.love@gmail.com>
2026-06-28 23:22:07 +0800

    perf: optimize window background color

--- src/ShadUI/Converters/PlatformConverters.cs
+++ src/ShadUI/Converters/PlatformConverters.cs
@@ -16,23 +16,11 @@ public static class PlatformConverters
 
     private sealed class WindowBackgroundConverterImpl : IMultiValueConverter
     {
-        // TODO: Cross-platform opacity handling
-        private static double Opacity
-        {
-            get
-            {
-                var osVersion = Environment.OSVersion;
-                return osVersion.Platform switch
-                {
-                    // 0 for Windows 11, 0.9 for Windows 10
-                    PlatformID.Win32NT when osVersion.Version >= new Version(10, 0, 22000) => 0.0,
-                    PlatformID.Win32NT => 0.7,
-                    PlatformID.MacOSX => 0.6,
-                    _ => 1.0
-                };
-            }
-        }
-        
+        private static double Opacity => OperatingSystem.IsWindowsVersionAtLeast(10, 0, 22000) ? 0.0 : // full mica
+            OperatingSystem.IsWindows() ? 0.8 :
+            OperatingSystem.IsMacOS() ? 0.95 :
+            1.0;
+
         public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
         {
             return values is not [Color color] ? null : new SolidColorBrush(color, opacity: Opacity);
