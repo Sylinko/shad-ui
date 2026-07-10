ee67efb8e0b6a6a0a37bf3c117b5fb0f06971e10
DearVa <dear.va.love@gmail.com>
2026-07-10 20:24:48 +0800

    feat: add IDataTemplates

---
+++ src/ShadUI/Utilities/IDataTemplates.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+
+namespace ShadUI;
+
+/// <summary>
+/// Represents a collection of data templates that can be used to build controls for different types of data.
+/// </summary>
+public class IDataTemplates : DataTemplates, IDataTemplate
+{
+    public Control? Build(object? param) => this.FirstOrDefault(x => x.Match(param))?.Build(param);
+
+    public bool Match(object? data) => this.Any(x => x.Match(data));
+}
