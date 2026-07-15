39b303c7b0948c7f33c6880388362d280fb97ba0
DearVa <dear.va.love@gmail.com>
2026-07-15 12:16:47 +0800

    perf: optimize `ToolTip` styles

--- src/ShadUI/Themes/ShadTheme.axaml.cs
+++ src/ShadUI/Themes/ShadTheme.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Styling;
 using ShadUI.Extensions;
@@ -15,6 +16,39 @@ public class ShadTheme : Styles
     {
         ToolTip.ShowDelayProperty.ForceOverrideDefaultValue(typeof(Control), 50);
         ToolTip.PlacementProperty.ForceOverrideDefaultValue(typeof(Control), PlacementMode.Top);
+        ToolTip.VerticalOffsetProperty.ForceOverrideDefaultValue(typeof(Control), 0d);
+        ToolTip.PlacementProperty.Changed.AddClassHandler<Control>(HandleToolTipPlacementChanged);
+    }
+
+    private static void HandleToolTipPlacementChanged(Control sender, AvaloniaPropertyChangedEventArgs args)
+    {
+        switch (args.NewValue)
+        {
+            case PlacementMode.Top:
+            {
+                sender.SetValue(ToolTip.VerticalOffsetProperty, -3);
+                sender.SetValue(ToolTip.HorizontalOffsetProperty, 0);
+                break;
+            }
+            case PlacementMode.Bottom:
+            {
+                sender.SetValue(ToolTip.VerticalOffsetProperty, 3);
+                sender.SetValue(ToolTip.HorizontalOffsetProperty, 0);
+                break;
+            }
+            case PlacementMode.Left:
+            {
+                sender.SetValue(ToolTip.VerticalOffsetProperty, 0);
+                sender.SetValue(ToolTip.HorizontalOffsetProperty, -3);
+                break;
+            }
+            case PlacementMode.Right:
+            {
+                sender.SetValue(ToolTip.VerticalOffsetProperty, 0);
+                sender.SetValue(ToolTip.HorizontalOffsetProperty, 3);
+                break;
+            }
+        }
     }
 
     /// <summary>
