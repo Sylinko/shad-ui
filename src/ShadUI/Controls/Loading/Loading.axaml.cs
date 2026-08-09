37247f3b2cbb50cd5cfca120e6045598e3369642
DearVa <dear.va.love@gmail.com>
2026-08-10 00:47:12 +0800

    feat: add `Size` property for `Loading`

--- src/ShadUI/Controls/Loading/Loading.axaml.cs
+++ src/ShadUI/Controls/Loading/Loading.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls.Primitives;
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
 
 // ReSharper disable once CheckNamespace
 namespace ShadUI;
@@ -8,4 +9,18 @@ namespace ShadUI;
 /// </summary>
 public class Loading : TemplatedControl
 {
+    public static readonly StyledProperty<double> SizeProperty =
+        AvaloniaProperty.Register<Loading, double>(nameof(Size), 24d);
+
+    public double Size
+    {
+        get => GetValue(SizeProperty);
+        set => SetValue(SizeProperty, value);
+    }
+
+    public Loading()
+    {
+        this[!WidthProperty] = this[!SizeProperty];
+        this[!HeightProperty] = this[!SizeProperty];
+    }
 }
