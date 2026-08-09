fa8dda75b56184ad702acaff1b944069dbd0c685
DearVa <dear.va.love@gmail.com>
2026-08-10 00:47:29 +0800

    feat: add `EmptyControlTemplate` and `EmptyDataTemplate`

---
+++ src/ShadUI/Utilities/EmptyControlTemplate.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+
+namespace ShadUI;
+
+/// <summary>
+/// A control template that always throws a NotImplementedException when attempting to build a control, effectively indicating that no control can be built from this template.
+/// </summary>
+public sealed class EmptyControlTemplate : IControlTemplate
+{
+    public static EmptyControlTemplate Shared { get; } = new();
+
+    public TemplateResult<Control>? Build(TemplatedControl param) => null;
+}
---
+++ src/ShadUI/Utilities/EmptyDataTemplate.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+
+namespace ShadUI;
+
+/// <summary>
+/// A data template that always returns null, effectively rendering nothing.
+/// </summary>
+public sealed class EmptyDataTemplate : IDataTemplate
+{
+    public static EmptyDataTemplate Shared { get; } = new();
+
+    public Control? Build(object? param) => null;
+
+    public bool Match(object? data) => true;
+}
